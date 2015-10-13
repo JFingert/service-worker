4157ec64dabb6a9c648e2127704b302b39958bb8
JFingert <joshuafingert@gmail.com>
2015-10-13 10:44:28 -0700

    initial commit

---
+++ public/doWork2.js
@@ -0,0 +1,16 @@
+importScripts('cache-polyfill/index.js');
+self.addEventListener('message', function(e) {
+  var data = e.data;
+  switch (data.cmd) {
+    case 'start':
+      self.postMessage('WORKER STARTED: ' + data.msg);
+      break;
+    case 'stop':
+      self.postMessage('WORKER STOPPED: ' + data.msg +
+                       '. (buttons will no longer work)');
+      self.close(); // Terminates the worker.
+      break;
+    default:
+      self.postMessage('Unknown command: ' + data.msg);
+  };
+}, false);
