8f54ae857d40a6b364cd497508e5ae792b490b3d
JFingert <joshuafingert@gmail.com>
2015-10-13 13:15:22 -0700

    trying setup#2

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 
-function sayHI() {
-  worker.postMessage({'cmd': 'start', 'msg': 'Hi'});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/doWork2.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
 }
 
-function stop() {
-  // worker.terminate() from this script would also stop the worker.
-  worker.postMessage({'cmd': 'stop', 'msg': 'Bye'});
-}
 
-function unknownCmd() {
-  worker.postMessage({'cmd': 'foobard', 'msg': '???'});
-}
 
-var worker = new Worker('doWork2.js');
 
-worker.addEventListener('message', function(e) {
-  document.getElementById('result').textContent = e.data;
-}, false);
+
+
+
+
+
+// function sayHI() {
+//   worker.postMessage({'cmd': 'start', 'msg': 'Hi'});
+// }
+
+// function stop() {
+//   // worker.terminate() from this script would also stop the worker.
+//   worker.postMessage({'cmd': 'stop', 'msg': 'Bye'});
+// }
+
+// function unknownCmd() {
+//   worker.postMessage({'cmd': 'foobard', 'msg': '???'});
+// }
+
+// var worker = new Worker('doWork2.js');
+
+// worker.addEventListener('message', function(e) {
+//   document.getElementById('result').textContent = e.data;
+// }, false);
